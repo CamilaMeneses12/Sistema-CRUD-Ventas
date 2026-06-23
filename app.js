97c9ba4ca83b40054a5d9ea8671fe7f9304383b9
CamilaMeneses12 <menesescamila565@gmail.com>
2026-06-23 08:16:55 -0500

    Comentarios

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import express from "express";
-import ventasRouter from "./routes/ventas.routes.js";
+import express from "express"; 
+import ventasRouter from "./routes/ventas.routes.js";//Importa el archivo donde están definidas todas las rutas de la API.
 
 const app = express();
 const port = 4000;
 
-app.use(express.json()); // Indicando que vamos a trabajar con formato json
-app.use("/tienda", ventasRouter);
+app.use(express.json()); // permite recibir datos en formato JSON en el body
+app.use("/tienda", ventasRouter); //todas las rutas empiezan con /tienda
 
 app.listen(port, () => {
     console.log(`Aplicacion corriendo en el puerto ${port}`);
